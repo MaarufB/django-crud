0ea7929ab1dd8a5e0c38e5f1595605434b793ea0
Ma-aruf M. Burad <burad.m@AD.SPROBE.PH>
2022-05-19 17:48:31 +0800

    continue at home

--- employee/models.py
+++ employee/models.py
@@ -5,7 +5,7 @@ class Employees(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    contact = models.IntegerField(max_length=12)
-    
+    contact = models.IntegerField(null=True)
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Employees
 
 # Create your views here.
 def test(request):
@@ -19,7 +20,11 @@ def delete_employee(request):
     pass
 
 def get_employees(request):
-    pass
+    data = Employees.objects.all()
+    # print(data)
+    context = {'employees': data}
+    return render(request, template_name='employee/test.html', context=context)
+
 
 def get_employee(request, id):
     pass
